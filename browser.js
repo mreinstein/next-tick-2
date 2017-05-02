712db03966e68b043f7d2984e555832be58fa587
Mike Reinstein <reinstein.mike@gmail.com>
2017-05-02 10:56:15 -0700

    even smaller bundles

--- browser.js
+++ browser.js
@@ -1,62 +1,62 @@
-'use strict';
+'use strict'
 
 var ensureCallable = function (fn) {
-	if (typeof fn !== 'function') throw new TypeError(fn + " is not a function");
-	return fn;
-};
+	if (typeof fn !== 'function') throw new TypeError(fn + " is not a function")
+	return fn
+}
 
 var byObserver = function (Observer) {
-	var node = document.createTextNode(''), queue, currentQueue, i = 0;
+	var node = document.createTextNode(''), queue, currentQueue, i = 0
 	new Observer(function () {
-		var callback;
+		var callback
 		if (!queue) {
-			if (!currentQueue) return;
-			queue = currentQueue;
+			if (!currentQueue) return
+			queue = currentQueue
 		} else if (currentQueue) {
-			queue = currentQueue.concat(queue);
+			queue = currentQueue.concat(queue)
 		}
-		currentQueue = queue;
-		queue = undefined;
+		currentQueue = queue
+		queue = undefined
 		if (typeof currentQueue === 'function') {
-			callback = currentQueue;
-			currentQueue = undefined;
-			callback();
-			return;
+			callback = currentQueue
+			currentQueue = undefined
+			callback()
+			return
 		}
-		node.data = (i = ++i % 2); // Invoke other batch, to handle leftover callbacks in case of crash
+		node.data = (i = ++i % 2) // Invoke other batch, to handle leftover callbacks in case of crash
 		while (currentQueue) {
-			callback = currentQueue.shift();
-			if (!currentQueue.length) currentQueue = undefined;
-			callback();
+			callback = currentQueue.shift()
+			if (!currentQueue.length) currentQueue = undefined
+			callback()
 		}
-	}).observe(node, { characterData: true });
+	}).observe(node, { characterData: true })
 	return function (fn) {
-		ensureCallable(fn);
+		ensureCallable(fn)
 		if (queue) {
-			if (typeof queue === 'function') queue = [queue, fn];
-			else queue.push(fn);
-			return;
+			if (typeof queue === 'function') queue = [queue, fn]
+			else queue.push(fn)
+			return
 		}
-		queue = fn;
-		node.data = (i = ++i % 2);
-	};
-};
+		queue = fn
+		node.data = (i = ++i % 2)
+	}
+}
 
 module.exports = (function () {
 	// MutationObserver
 	if ((typeof document === 'object') && document) {
-		if (typeof MutationObserver === 'function') return byObserver(MutationObserver);
-		if (typeof WebKitMutationObserver === 'function') return byObserver(WebKitMutationObserver);
+		if (typeof MutationObserver === 'function') return byObserver(MutationObserver)
+		if (typeof WebKitMutationObserver === 'function') return byObserver(WebKitMutationObserver)
 	}
 
 	// W3C Draft
 	// http://dvcs.w3.org/hg/webperf/raw-file/tip/specs/setImmediate/Overview.html
 	if (typeof setImmediate === 'function') {
-		return function (cb) { setImmediate(ensureCallable(cb)); };
+		return function (cb) { setImmediate(ensureCallable(cb)) }
 	}
 
 	// Wide available standard
 	if ((typeof setTimeout === 'function') || (typeof setTimeout === 'object')) {
-		return function (cb) { setTimeout(ensureCallable(cb), 0); };
+		return function (cb) { setTimeout(ensureCallable(cb), 0) }
 	}
-}());
+}())
